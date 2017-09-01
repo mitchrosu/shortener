79df07501c1b34e880794756b8ae4859caeec9b7
mitchrosu <mitchrosu@gmail.com>
2017-09-01 13:16:53 -0500

    Rails 5.1 Params Compatibility Fix
    Rails 5.1 has an issue with param permissions, override the check.

--- app/controllers/shortener/shortened_urls_controller.rb
+++ app/controllers/shortener/shortened_urls_controller.rb
@@ -4,9 +4,10 @@ class Shortener::ShortenedUrlsController < ActionController::Base
   include Shortener
 
   def show
-    token = ::Shortener::ShortenedUrl.extract_token(params[:id])
+  	safe_params = params.to_unsafe_h
+    token = ::Shortener::ShortenedUrl.extract_token(safe_params[:id])
     track = Shortener.ignore_robots.blank? || request.human?
-    url   = ::Shortener::ShortenedUrl.fetch_with_token(token: token, additional_params: params, track: track)
+    url   = ::Shortener::ShortenedUrl.fetch_with_token(token: token, additional_params: safe_params, track: track)
     redirect_to url[:url], status: :moved_permanently
   end
 end
